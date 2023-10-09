d42a61753f31a5f205280b9718eb1f634e537380
ash <ashmercesletifercoc@gmail.com>
2023-10-09 21:55:47 +0530

    added function

--- program3.c
+++ program3.c
@@ -1,33 +1,63 @@
-//linear search
+//Binary search
 
 #include<stdio.h>
+
+int binarySearch(int arr[], int n, int s) {
+    
+    int l=0;
+	int r=n-1;
+	
+	while(l<=r){
+
+		int mid = (l+r)/2;
+	
+		if(s==arr[mid]){
+
+            return mid;
+
+		}
+
+		else if(s<arr[mid]){
+			r=mid-1;
+		}
+		
+		else{
+			l=mid+1;
+		}
+	}
+
+    return -1;
+}
+
 int main(){
 
 	int n,s,count=0;
-	printf("Enter size of the array: ");
+	printf("Enter size of array: ");
 	scanf("%d",&n);
 	int arr[n];
+	
 	for(int i=0;i<n;i++){
-
-		printf("Enter array element at index number %d: ",i);
+	
+		printf("Enter the array element at index number %d: ",i);
 		scanf("%d",&arr[i]);
 	}
 	
-	printf("Enter the search element: ");
+	printf("Enter search element: ");
 	scanf("%d",&s);
-	
-	for(int i=0;i<n;i++){
-	
-		if(arr[i]==s){
-			printf("Search element found at index number: %d",i);
-			count+=1;
-			break;
-			}
-		}
 
-	if(count==0){
-		printf("Search element not found!");
-	}
-			
+
+    int result = binarySearch(arr, n, s);
+
+    if (result!=-1) {
+        printf("Element found at index number: %d", result);
+    } 
+    
+    else {
+        printf("Element not found!");
+    }
+
+
 	return 0;
 }
+
+
